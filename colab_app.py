a27b530b85636c250a04c701592e5216e101083b
tori29umai <72191117+tori29umai0123@users.noreply.github.com>
2023-07-19 15:28:22 +0900

    v1.0

--- colab_app.py
+++ colab_app.py
@@ -9,20 +9,24 @@ from Scripts import ShadowGenerator  # taggerモジュールをインポート
 import os
 from datetime import datetime
 
-def shadow_generation(init_image: Image, max_size: str,prompt: str):
+def image_generation(init_image: Image, max_size: str,prompt: str, strength:str):
     max_size = int(max_size)
+    strength = float(strength)
+
     mode = "MaskOFF"
     output_folder = "output"
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     output_filename = f"output_image_{timestamp}.png"
-    output_image = ShadowGenerator.main(init_image, mask_image, mode , max_size=max_size,prompt=prompt)
+    output_image = Kakikomi.main(init_image, mask_image, mode , max_size=max_size,prompt=prompt, strength=strength)
     output_image.save(os.path.join(output_folder, output_filename))
     return output_image
 
-def shadow_generation_Mask(init_image: Image, mask_image: Image, max_size: str, prompt: str):
+def image_generation_Mask(init_image: Image, mask_image: Image, max_size: str, prompt: str, strength:str):
     max_size = int(max_size)
+    strength = float(strength)
+
     if mask_image == None:
         mode = "MaskOFF"
     else:
@@ -32,19 +36,7 @@ def shadow_generation_Mask(init_image: Image, mask_image: Image, max_size: str, 
         os.makedirs(output_folder)
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     output_filename = f"output_image_{timestamp}.png"
-    output_image = ShadowGenerator.main(init_image, mask_image, mode, max_size=max_size, prompt=prompt)
-    output_image.save(os.path.join(output_folder, output_filename))
-    return output_image
-
-def shadow_generation_Normalmap(init_image: Image, max_size: str,prompt: str):
-    max_size = int(max_size)
-    mode = "Normalmap"
-    output_folder = "output"
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    output_filename = f"output_image_{timestamp}.png"
-    output_image = ShadowGenerator.main(init_image, mask_image, mode , max_size=max_size,prompt=prompt)
+    output_image = Kakikomi.main(init_image, mask_image, mode, max_size=max_size, prompt=prompt, strength=strength)
     output_image.save(os.path.join(output_folder, output_filename))
     return output_image
 
@@ -64,58 +56,44 @@ def prompt_generation(image: Image):
 
 
 with gr.Blocks() as ui:
-    with gr.Tab("LinetoShadow"):
+    with gr.Tab("KakikomiImage"):
         with gr.Row():
             with gr.Column():
-                input_image = gr.Image(type="pil",label="LineArtImage")
+                input_image = gr.Image(type="pil",label="PreImage")
                 with gr.Row():
                     max_size = gr.Textbox(label="Enter max_size",value = "960")
+                    strength = gr.Slider(minimum=0, maximum=1, value=0.4, step=0.1, label="strength", interactive=True )
 
             with gr.Column():
-                prompt = gr.Textbox(label="Enter prompt",lines = 5)
+                prompt = gr.Textbox(label="prompt",lines = 5)
                 prompt_btn = gr.Button("PromptGenerate")
                 prompt_btn.click(fn=prompt_generation, inputs=input_image, outputs=prompt)
                 mask_image = None
                 output=gr.Image(elem_id="output_image")
-                inputs=[input_image,max_size,prompt]
-                shadow_btn = gr.Button("ShadowGenerate")
-                shadow_btn.click(fn=shadow_generation, inputs=inputs, outputs=output)
+                inputs=[input_image,max_size,prompt,strength]
+                image_btn = gr.Button("ImageGenerate")
+                image_btn.click(fn=image_generation, inputs=inputs, outputs=output)
 
     with gr.Tab("Inpaint"):
         with gr.Row():
             with gr.Column():
-                input_image = gr.Image(type="pil",label="LineArtImage")
+                input_image = gr.Image(type="pil",label="PreImage")
                 mask_image = gr.Image(type="pil",label="MaskImage")
                 mask_btn = gr.Button("MaskGenerate")
                 mask_btn.click(fn=mask_generation, inputs=input_image, outputs=mask_image)
                 with gr.Row():
                     max_size = gr.Textbox(label="Enter max_size",value = "960")
+                    strength = gr.Slider(minimum=0, maximum=1, value=0.4, step=0.1, label="strength", interactive=True )
+
 
             with gr.Column():
-                prompt = gr.Textbox(label="Enter prompt",lines = 5)
+                prompt = gr.Textbox(label="prompt",lines = 5)
                 prompt_btn = gr.Button("PromptGenerate")
                 prompt_btn.click(fn=prompt_generation, inputs=input_image, outputs=prompt)
                 output=gr.Image(elem_id="output_image")
-                inputs=[input_image,mask_image,max_size,prompt]
-                shadow_btn = gr.Button("ShadowGenerate")
-                shadow_btn.click(fn=shadow_generation_Mask, inputs=inputs, outputs=output)
-
-    with gr.Tab("Normalmap"):
-        with gr.Row():
-            with gr.Column():
-                input_image = gr.Image(type="pil",label="LineArtImage")
-                with gr.Row():
-                    max_size = gr.Textbox(label="Enter max_size",value = "960")
-
-            with gr.Column():
-                prompt = gr.Textbox(label="Enter prompt",lines = 5)
-                prompt_btn = gr.Button("PromptGenerate")
-                prompt_btn.click(fn=prompt_generation, inputs=input_image, outputs=prompt)
-                mask_image = None
-                output=gr.Image(elem_id="output_image")
-                inputs=[input_image,max_size,prompt]
-                shadow_btn = gr.Button("ShadowGenerate")
-                shadow_btn.click(fn=shadow_generation_Normalmap, inputs=inputs, outputs=output)
+                inputs=[input_image,mask_image,max_size,prompt,strength]
+                image_btn = gr.Button("ImageGenerate")
+                image_btn.click(fn=image_generation_Mask, inputs=inputs, outputs=output)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
